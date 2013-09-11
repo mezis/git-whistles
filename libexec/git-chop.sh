03a43f9e265556f177e776fda54447e628dc2276
David Silva <davidslv@gmail.com>
2013-09-11 16:16:49 +0100

    Allows possibility of deleting several branches
    This commit intends to allow passing several branches to be deleted
    Usage: git chop [branch1] [branch2]

--- libexec/git-chop.sh
+++ libexec/git-chop.sh
@@ -12,13 +12,14 @@ die() {
 head=$(git symbolic-ref HEAD 2> /dev/null || git log -1 --format=%h)
 current_branch=${head#refs/heads/}
 
-branch="$1"
-: ${branch:-$head}
-echo "Closing feature branch $branch"
+for branch in "$@" ; do
+  : ${branch:-$head}
+  echo "Closing feature branch $branch"
 
-if [ "$branch" = "$current_branch" ] ; then
-  git checkout master || die
-fi
+  if [ "$branch" = "$current_branch" ] ; then
+    git checkout master || die
+  fi
 
-git branch -D "$branch" || die
-git push origin ":$branch" || die
+  git branch -D "$branch" || die
+  git push origin ":$branch" || die
+done
