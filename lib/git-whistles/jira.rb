0b6107041a514cf0dcacf026788697fe7c577f68
Martin Phee <martyphee@gmail.com>
2016-02-05 10:24:19 -0600

    Made updates per feedback.

--- lib/git-whistles/jira.rb
+++ lib/git-whistles/jira.rb
@@ -1,45 +1,17 @@
 module Git
   module Whistles
-    module Jira
-      def get_config
-        username = `git config jira.username`.strip
-        if username.empty?
-          puts Term::ANSIColor.yellow %Q{
-        Your branch appears to have a issue ID,
-        but I don't know your JIRA username!
-        Please set it with:
-        $ git config [--global] jira.username <username>
-      }
-          die "Aborting."
-        end
+    class Jira
+      attr_reader :username, :password, :site
 
-        password = `git config jira.password`.strip
-        if password.empty?
-          puts Term::ANSIColor.yellow %Q{
-        Your branch appears to have a issue ID,
-        but I don't know your JIRA password!
-        Please set it with:
-        $ git config [--global] jira.password <password>
-      }
-          die "Aborting."
-        end
-
-        site = `git config jira.site`.strip
-        if site.empty?
-          puts Term::ANSIColor.yellow %Q{
-        Your branch appears to have a issue ID,
-        but I don't know your JIRA site!
-        Please set it with:
-        $ git config [--global] jira.site <https://mydomain.atlassian.net>
-      }
-          die "Aborting."
-        end
-
-        { username: username, password: password, site: site }
+      def initialize
+        get_config
       end
 
-      def get_client(opts = get_config)
+      def get_client(opts = {})
         options = {
+          username: @username,
+          password: @password,
+          site: @site,
           context_path: '',
           auth_type: :basic,
           read_timeout: 120
@@ -48,6 +20,42 @@ module Git
         options.merge!(opts)
 
         JIRA::Client.new(options)
+      end
+
+      def get_config
+        @username = `git config jira.username`.strip
+        if username.empty?
+          puts Term::ANSIColor.yellow %Q{
+            Your branch appears to have a issue ID,
+            but I don't know your JIRA username!
+            Please set it with:
+            $ git config [--global] jira.username <username>
+          }
+          die "Aborting."
+        end
+
+        @password = `git config jira.password`.strip
+        if password.empty?
+          puts Term::ANSIColor.yellow %Q{
+            Your branch appears to have a issue ID,
+            but I don't know your JIRA password!
+            Please set it with:
+            $ git config [--global] jira.password <password>
+          }
+          die "Aborting."
+        end
+
+        @site = `git config jira.site`.strip
+        if site.empty?
+          puts Term::ANSIColor.yellow %Q{
+            Your branch appears to have a issue ID,
+            but I don't know your JIRA site!
+            Please set it with:
+            $ git config [--global] jira.site <https://mydomain.atlassian.net>
+          }
+          die "Aborting."
+        end
+
       end
     end
   end
